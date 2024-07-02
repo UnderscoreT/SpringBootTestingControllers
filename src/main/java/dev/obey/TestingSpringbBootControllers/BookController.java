c40795bf3fc53bce2ff49a688bcb0a2b69679418
OBEY <obeyblessing@gmail.com>
2024-07-02 15:04:51 +0200

    mvc tests

--- src/main/java/dev/obey/TestingSpringbBootControllers/BookController.java
+++ src/main/java/dev/obey/TestingSpringbBootControllers/BookController.java
@@ -1,4 +1,29 @@
 package dev.obey.TestingSpringbBootControllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book findOne(@PathVariable int id) {
+        return bookService.findOne(id);
+    }
+
 }
